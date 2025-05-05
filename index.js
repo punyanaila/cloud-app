a27cbbaccf760f049d41299746a4de372d0ca4e8
punyanaila <naila.sahda18@gmail.com>
2025-05-05 13:59:34 +0700

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Railway + Cloud Computing!");
+  res.send("Hello Pak Johan");
 });
 
 app.listen(port, () => {
